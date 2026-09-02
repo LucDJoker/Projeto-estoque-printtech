3274c6055134aa3755d819a10cee4f46f584d1e4
Lucas David <lvialle87@gmail.com>
2026-09-02 19:38:06 -0300

    Adiciona Lista estoque e for

--- gerenciador-estoque-3d.js
+++ gerenciador-estoque-3d.js
@@ -4,6 +4,11 @@ let estoque = [
     { nome: "Base controle Caveira 3D", quantidade: 10, preco: 150.00 },
     { nome: "Suporte Parede Headset 3D", quantidade: 5, preco: 80.00 },
     { nome: "Vaso Decorativo 3D", quantidade: 20, preco: 45.00 },
-    
-    
+    { nome: "Luminária 3D", quantidade: 15, preco: 120.00 },
+    { nome: "Porta Canetas 3D", quantidade: 25, preco: 30.00 },
+    { nome: "Miniatura Personagem 3D", quantidade: 8, preco: 60.00 },
 ]
+for (let i = 0; i < estoque.length; i++) {
+    console.log(estoque[i].nome);
+    console.log(estoque[i].preco);
+}
