9e7a7b9a0b3d7ddc7893d9ba52554616bf0dc204
Lucas David <lvialle87@gmail.com>
2026-09-02 19:23:42 -0300

    Adiciona readline-sync

--- gerenciador-estoque-3d.js
+++ gerenciador-estoque-3d.js
@@ -1,1 +1,9 @@
-const entrada = require('readline-sync');
+const entrada = require('readline-sync')();
+
+let estoque = [
+    { nome: "Base controle Caveira 3D", quantidade: 10, preco: 150.00 },
+    { nome: "Suporte Parede Headset 3D", quantidade: 5, preco: 80.00 },
+    { nome: "Vaso Decorativo 3D", quantidade: 20, preco: 45.00 },
+    
+    
+]
