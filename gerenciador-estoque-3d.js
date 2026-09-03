5c1e1064fd8252034df476552ffded0f1d0ccabe
Lucas David <lvialle87@gmail.com>
2026-09-02 21:20:11 -0300

    Atualização projeto

--- gerenciador-estoque-3d.js
+++ gerenciador-estoque-3d.js
@@ -1,6 +1,4 @@
-const entrada = require('readline-sync')();
-
-let estoque = [
+let catalogoPrintTech = [
     { nome: "Base controle Caveira 3D", quantidade: 10, preco: 150.00 },
     { nome: "Suporte Parede Headset 3D", quantidade: 5, preco: 80.00 },
     { nome: "Vaso Decorativo 3D", quantidade: 20, preco: 45.00 },
@@ -8,7 +6,10 @@ let estoque = [
     { nome: "Porta Canetas 3D", quantidade: 25, preco: 30.00 },
     { nome: "Miniatura Personagem 3D", quantidade: 8, preco: 60.00 },
 ]
-for (let i = 0; i < estoque.length; i++) {
-    console.log(estoque[i].nome);
-    console.log(estoque[i].preco);
+function adicionarProduto(nome, quantidade, preco) {
+    catalogoPrintTech.push({ nome: nome, quantidade: quantidade, preco: preco });
+}
+for (let i = 0; i < catalogoPrintTech.length; i++) {
+    console.log(catalogoPrintTech[i].nome);
+    console.log(catalogoPrintTech[i].preco);
 }
